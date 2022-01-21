6e3a8f42581c8983636c6d5dec808d6c4414875d
Puneet Singh <er.puneet24@gmail.com>
2022-01-21 11:42:10 +0530

    Added env variable for dev/prod path

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,8 +10,7 @@ const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const pathArray = location.pathname.split("/")
-  // const origin = "https://mojoauth.com/docs/"
-  const origin = "https://mj-dev-docs.netlify.app/docs/"
+  const origin = process.env.SITE_URL +"docs/"
   return (
     <div className="global-wrapper">
       <div id="root">
