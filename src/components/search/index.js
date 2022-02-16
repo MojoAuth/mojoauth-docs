bb41e12ad22a11ba4ba7383cfd1596d014624da6
narendra-pareek <narendra.pareek@loginradius.com>
2022-02-16 12:21:16 +0530

    Implemented algolia search

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,19 +1,12 @@
 import algoliasearch from "algoliasearch/lite"
 import { createRef, default as React, useState, useMemo } from "react"
 import { InstantSearch } from "react-instantsearch-dom"
-import { ThemeProvider } from "styled-components"
-import StyledSearchBox from "./styled-search-box"
+
+import SearchBox from "./styled-search-box"
 import StyledSearchResult from "./styled-search-result"
-import StyledSearchRoot from "./styled-search-root"
 import useClickOutside from "./use-click-outside"
-
-const theme = {
-  foreground: "#050505",
-  background: "white",
-  faded: "#888",
-}
-
-export default function Search({ indices }) {
+import styles from "./search.module.css"
+const Search = ({ indices }) => {
   const rootRef = createRef()
   const [query, setQuery] = useState()
   const [hasFocus, setFocus] = useState(false)
@@ -29,20 +22,21 @@ export default function Search({ indices }) {
   useClickOutside(rootRef, () => setFocus(false))
 
   return (
-    <ThemeProvider theme={theme}>
-      <StyledSearchRoot ref={rootRef}>
-        <InstantSearch
-          searchClient={searchClient}
-          indexName={indices[0].name}
-          onSearchStateChange={({ query }) => setQuery(query)}
-        >
-          <StyledSearchBox onFocus={() => setFocus(true)} hasFocus={hasFocus} />
-          <StyledSearchResult
-            show={query && query.length > 0 && hasFocus}
-            indices={indices}
-          />
-        </InstantSearch>
-      </StyledSearchRoot>
-    </ThemeProvider>
+    <div className="search" ref={rootRef}>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName={indices[0].name}
+        onSearchStateChange={({ query }) => setQuery(query)}
+      >
+        <SearchBox onFocus={() => setFocus(true)} hasFocus={hasFocus} />
+        <StyledSearchResult
+          className={styles.searchResult}
+          show={query && query.length > 0 && hasFocus}
+          indices={indices}
+        />
+      </InstantSearch>
+    </div>
   )
 }
+
+export default Search
